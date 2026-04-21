1b4a719b050789dbf6666dd09c10b260fb7f12f9
dorareddy875-tech <dorare875@gmail.com>
2026-04-21 09:17:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page refresh
+
+  const username = document.getElementById("username").value.trim();
+  const password = document.getElementById("password").value.trim();
+
+  // Correct credentials
+  const correctUsername = "AMMU";
+  const correctPassword = "ammu22";
+
+  if (username === correctUsername && password === correctPassword) {
+    // Redirect to Yes/No page
+    window.location.href = "page1.html";
+  } else {
+    alert("Incorrect Username or Password ❌");
+  }
+});
